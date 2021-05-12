cca301b028e44441122d4cdc06e9b25859a75c7f
Renato de Souza <renbstux@gmail.com>
2021-05-12 11:37:41 -0300

    Sending JWT Token in Swagger

--- src/main/java/br/com/renbstux/forum/config/swagger/SwaggerUiWebMvcConfigurer.java
+++ src/main/java/br/com/renbstux/forum/config/swagger/SwaggerUiWebMvcConfigurer.java
@@ -1,11 +1,15 @@
 package br.com.renbstux.forum.config.swagger;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.renbstux.forum.modelo.Usuario;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -13,16 +17,22 @@ import springfox.documentation.spring.web.plugins.Docket;
 public class SwaggerUiWebMvcConfigurer {
 	
 	@Bean
-	public Docket forumApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
-		        .select() 
-		        .apis(RequestHandlerSelectors.basePackage("br.com.renbstux.forum")) 
-		        .paths(PathSelectors.ant("/**")) 
-		        .build() 
-		        .ignoredParameterTypes(Usuario.class);
-		
-	}
-	
-	
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.renbstux.forum"))
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                .ignoredParameterTypes(Usuario.class)
+                .globalOperationParameters(
+                        Arrays.asList(
+                                new ParameterBuilder()
+                                    .name("Authorization")
+                                    .description("Header para Token JWT")
+                                    .modelRef(new ModelRef("string"))
+                                    .parameterType("header")
+                                    .required(false)
+                                    .build()));
+    }
 
 }
